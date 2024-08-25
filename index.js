dabe50632fc163431b5e94f9648c7d20d327e7d9
mentalhealf <147326241+mentalhealf@users.noreply.github.com>
2024-08-25 19:25:19 +0100

    Finish

---
+++ index.js
@@ -0,0 +1,35 @@
+//1.  Вивести result в консоль.
+// Перевірити тип змінної result= 5+5+’5’.
+
+// let result = 5+5+'5'
+// console.log(result);
+// console.log(typeof(result));
+
+//2. Створіть змінну email з вашою електронною адресою. Напишіть скрипт,
+// який перевіряє чи містить змінна email символ @ і рахує загальну кількість символів. Результат виведіть в консоль.
+
+// let myEmail = "alexandrnick@gmail.com";
+// console.log(myEmail.includes("@"));
+// console.log(myEmail.length);
+
+//3. Записати в окремих змінних кожне слово: My, name, is. Поєднати всі слова в змінну fullName.
+// До змінної fullName додати ‘Viktor’. fullName вивести в консоль.
+
+// const my = "My";;
+// const myName = "name";
+// const is = "is";
+// const fullName = `Hello ${my} ${myName} ${is} Sasha.`
+// console.log(fullName);
+
+
+//4. Створіть змінну userName з ім’ям користувача і змінну payment з сумою до оплати.
+// За допомогою шаблонного рядка виведіть на екран через alert повідомлення на прикладі цього: «Дякуємо, Олександро! До сплати 300 гривень»
+
+// let userName = "Alex";
+// let paymentCheck = "20$";
+// let alertPaymentCheck = `Thank's, Alex for paying 20$ `;
+// alert(alertPaymentCheck);
+
+
+
+
